4723880b3836c878d88fee57eb4e392b7caa446e
unknown <xarus82@mail.ru>
2022-12-08 22:23:07 +0300

    Задача 39

---
+++ Task_39/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+const int SIZE = 10;
+const int LEFT_RANGE = 100;
+const int RIGHT_RANGE = 999;
+int[] array = new int[SIZE];
+array = FillArray(SIZE, LEFT_RANGE, RIGHT_RANGE);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+ReverseArray(array);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+void ReverseArray(int[] array)
+{
+    for(int i = 0; i < array.Length/2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
---
+++ Task_42/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+System.Console.WriteLine("Введите число: ");
+int N =Convert.ToInt32(Console.ReadLine());
+GetBinaryView(N);
+
+void GetBinaryView(int number)
+{
+    if(number <=0) 
+    {
+        return;
+    }
+    GetBinaryView(number/2);
+    System.Console.Write(number%2);
+}
